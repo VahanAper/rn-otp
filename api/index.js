7543c16e6f7c1b13847b307dde9a67a0ec69889a
Vahan Asryan <v.asryan@apollobytes.com>
2018-03-08 16:01:11 +0400

    handle submit

---
+++ api/index.js
@@ -0,0 +1,4 @@
+import { ROOT_URL } from './config';
+
+export const createUser = `${ROOT_URL}/createUser`;
+export const requestOneTimePassword = `${ROOT_URL}/requestOneTimePassword`;
--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -8,16 +8,47 @@ import {
     FormLabel,
     FormInput,
 } from 'react-native-elements';
+import axios from 'axios';
+
+import {
+    createUser,
+    requestOneTimePassword,
+} from '../api';
 
 export default class SignUpForm extends React.Component {
+    state = {
+        phone: '',
+    };
+    
+    setPhone = (phone) => {
+        this.setState({ phone });
+    }
+    
+    handleSubmit = async () => {
+        const { phone } = this.state;
+        
+        try {
+            await axios.post(createUser, { phone });
+            await axios.post(requestOneTimePassword, { phone });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    
     render() {
         return (
             <View>
                 <View style={{ marginBottom: 10 }}>
                     <FormLabel>Enter Phone Number</FormLabel>
-                    <FormInput />
+                    <FormInput
+                        value={this.state.phone}
+                        onChangeText={this.setPhone}
+                    />
                 </View>
-                <Button title="Submit" />
+                <Button
+                    title="Submit"
+                    onPress={this.handleSubmit}
+                />
             </View>
         );
     }
