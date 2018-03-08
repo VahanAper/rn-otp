f007e3e0a2b01ac6c6e13f9b525a6919f34a729e
Vahan Asryan <v.asryan@apollobytes.com>
2018-03-08 17:02:51 +0400

    create SignInForm with verify action, correct files

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
     render() {
         return (
             <View style={styles.container}>
                 <SignUpForm />
+                
+                <SignInForm />
             </View>
         );
     }
@@ -18,6 +21,6 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-around',
     },
 });
--- api/index.js
+++ api/index.js
@@ -2,3 +2,4 @@ import { ROOT_URL } from './config';
 
 export const createUser = `${ROOT_URL}/createUser`;
 export const requestOneTimePassword = `${ROOT_URL}/requestOneTimePassword`;
+export const verifyOneTimePassword = `${ROOT_URL}/verifyOneTimePassword`;
